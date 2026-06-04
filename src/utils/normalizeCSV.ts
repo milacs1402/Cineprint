1538c02a5d19d731cba12ca76ccc08dcca3ce77a
Camomila <camilacs1402@gmail.com>
2026-06-04 17:25:16 -0300

    feat(csv): add types and normalize functions

---
+++ src/utils/normalizeCSV.ts
@@ -0,0 +1,37 @@
+import type { Movie, NetflixMovie, LetterboxdMovie } from '../types';
+
+function normalizeNetflix(data: NetflixMovie[]) : Movie[] {
+
+    return data.map((movie) => {
+        return {
+            name : movie.Title,
+            watchedDate : movie.Date
+        }
+    })
+
+}
+
+function normalizeLetterboxd(data: LetterboxdMovie[]) : Movie[] {
+
+    return data.map((movie) => {
+        return {
+            name : movie.Name,
+            watchedDate : movie["Watched Date"],
+            year : movie.Year,
+            rating : movie.Rating
+        }
+    })
+}
+
+function detectSource(data: NetflixMovie[] | LetterboxdMovie[]) : 'netflix' | 'letterboxd' | 'data not found'{
+
+    if(data.length === 0) return 'data not found'
+    else {
+
+    if ('Title' in data[0]) return 'netflix'
+    else return 'letterboxd'
+
+    }
+}
+
+export { normalizeNetflix, normalizeLetterboxd, detectSource }
